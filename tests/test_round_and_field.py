f4e43cc9cb95d2882de5c296a513de66ec2788db
kaktyshiny <kaktyshiny@gmail.com>
2017-11-09 14:27:04 +0300

    testing

---
+++ tests/test_round_and_field.py
@@ -0,0 +1,38 @@
+def test_word_is_unsolved_on_start():
+    from hangman.round import Round
+
+    test_round = Round('python')
+
+    assert test_round.is_word_solved() == False
+
+
+def test_word_is_solved():
+    from hangman.round import Round
+
+    test_round = Round('python')
+    test_round.word_status = [True for _ in test_round.word]
+
+    assert test_round.is_word_solved() == True
+
+
+def test_game_over():
+    from hangman.round import Round
+    from hangman.field import HangmanField
+
+    test_round = Round('python')
+    max_tries = len(HangmanField().states)
+    test_round.tries = max_tries
+
+    assert test_round.is_lost() == True
+
+
+def test_draw_field():
+    from hangman.round import Round
+    from hangman.field import HangmanField
+
+    test_round = Round('python')
+    max_tries = len(HangmanField().states)
+
+    for i in range(1,len(HangmanField().states)):
+        last_len = len(HangmanField().states[i-1])
+        assert last_len < len(HangmanField().states[i])
