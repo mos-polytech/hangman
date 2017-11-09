2a357bb56b28e64afe70f05588e2db5d27d1a3e3
kaktyshiny <kaktyshiny@gmail.com>
2017-11-09 15:58:22 +0300

    testing players.py

--- tests/test_players.py
+++ tests/test_players.py
@@ -1,12 +1,21 @@
-def test_quess_new_word():
+def test_comp_quess_new_word():
     from hangman.players import ComputerPlayer
-    test_comp = ComputerPlayer
+    test_comp = ComputerPlayer()
 
     assert len(test_comp.quess_new_word()) > 0
 
 
-def test_should_change_turns:
+def test_comp_should_change_turns():
     from hangman.players import ComputerPlayer
-    test_comp = ComputerPlayer
+    test_comp = ComputerPlayer()
 
     assert test_comp.should_change_turns() is False
+
+
+def test_human_validate_word():
+    from hangman.players import HumanPlayer
+    test_human = HumanPlayer()
+
+    assert test_human._validate_word('') is False
+    assert test_human._validate_word('русскоеслово') is False
+    assert test_human._validate_word('python') is True
