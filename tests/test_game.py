424769f040dd2d068497323b5df588b4e4c2985f
kaktyshiny <kaktyshiny@gmail.com>
2017-11-09 16:35:53 +0300

    testing game.py

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,6 +1,12 @@
 def test_add_round():
     from hangman.game import Game
-    test_game = Game()
+    from hangman.round import Round
+    from hangman.players import HumanPlayer
 
-    test_game.add_round(1,1,2)
-    assert test_game.won_rounds[1] == 1
+    test_game = Game()
+    test_round = Round('python')
+    test_player_one = HumanPlayer()
+    test_player_two = HumanPlayer()
+
+    test_game.add_round(test_round,test_player_one,test_player_two)
+    assert test_game.won_rounds[test_player_one.id] == 1
