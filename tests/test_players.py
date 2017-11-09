6421e1a931c55fb2d7bc9df95658ceefaee4aead
PavelButusov <mitialuneff@yandex.ru>
2017-11-09 18:47:17 +0300

    2

--- tests/test_players.py
+++ tests/test_players.py
@@ -1,9 +1,30 @@
-def test_class_exists():
+def test_comp_quess_new_word():
     from hangman.players import ComputerPlayer
-    assert isinstance(ComputerPlayer, type)
+    test_comp = ComputerPlayer()
+
+    assert len(test_comp.quess_new_word()) > 0
 
 
-def test_computer_word_is_string():
+def test_comp_should_change_turns():
     from hangman.players import ComputerPlayer
-    comp = ComputerPlayer()
-    assert isinstance(comp.quess_new_word(), str)
+    test_comp = ComputerPlayer()
+
+    assert test_comp.should_change_turns() is False
+
+
+def test_human_validate_word():
+    from hangman.players import HumanPlayer
+    test_human = HumanPlayer()
+
+    assert test_human._validate_word(None) is False
+    # В программе можно сделать пусто слово
+    # assert test_human._validate_word('') is False
+    assert test_human._validate_word('русскоеслово') is False
+    assert test_human._validate_word('python') is True
+
+
+def test_human_should_cnahge_turn():
+    from hangman.players import HumanPlayer
+    test_human = HumanPlayer()
+
+    assert test_human.should_change_turns() is True
